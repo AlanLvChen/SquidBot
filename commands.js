d3a26174050934205039623d5e1bb72bec1cbdf7
Alan Chen <alan.lv.chen@gmail.com>
2021-02-25 04:09:45 -0800

    added roles and fixed command permissions

--- commands.js
+++ commands.js
@@ -1,7 +1,7 @@
 
 const unknownCommand = require("./commands/unknowncommand.js");
-const musicCommands = require("./commands/musicCommands.js");
-const generalCommands = require("./commands/generalCommands.js");
+const musicCommands = require("./commands/allUsers/musicCommands.js");
+const generalCommands = require("./commands/allUsers/generalCommands.js");
 const moderatorCommands = require("./commands/moderator/modCommands.js");
 
 
@@ -17,7 +17,7 @@ const commands = {
     queue: musicCommands,
     urban: generalCommands,
     flipcoin: generalCommands,
-    
+
 }
 
 //commands for mods
@@ -32,33 +32,30 @@ admin = '806731817474326578'
 mod = '806731247820079114'
 
 module.exports = async function (msg) {
-    //Currently only works in bot-testing channel
-    //if (msg.channel.id == "812777913791414282") {
-        let tokens = msg.content.split(" ");
-        let command = tokens.shift();
 
-        //if message starts with ! and after that isnt !!
-        if (command.charAt(0) == "!" && !(command.length == 1 || command.charAt(1).match(uglyRegex))) {
-            command = command.substring(1);
+    let tokens = msg.content.split(" ");
+    let command = tokens.shift();
 
+    //if message starts with ! and after that isnt !!
+    if (command.charAt(0) == "!" && !(command.length == 1 || command.charAt(1).match(uglyRegex))) {
+        command = command.substring(1);
+
+        //Mod commands work on every channel
+        if (command in modCommands && (msg.member.roles.cache.has(admin) || msg.member.roles.cache.has(mod))) {
+            console.log("Mod command was called");
+            modCommands[command](msg, tokens, command, Discord);
+        }
+        else if (msg.channel.id == "806732589204635678") {
             //if general command
             if (command in commands) {
                 console.log("General command was called");
                 commands[command](msg, tokens, command, Discord);
-            
-            //if mod command 
-            } else if (command in modCommands && (msg.member.roles.cache.has(admin) || msg.member.roles.cache.has(mod))) {
-                console.log("Mod command was called");
-                modCommands[command](msg, tokens, command, Discord);
-            
-            //else not a command
             } else {
                 commands["unknownCommand"](msg, Discord);
                 console.log(command);
             }
-            
-        }
-        
-    //}
-}
 
+        }
+
+    }
+}
--- commands/moderator/modCommands.js
+++ commands/moderator/modCommands.js
@@ -14,19 +14,25 @@ module.exports = async function (msg, args, cmd, Discord) {
     } 
     else if (cmd === 'roles') {
         const channel = '607063239037026344';
+        
         const leaguePing = msg.guild.roles.cache.find(role => role.name === "Pinged for League");
+        const leagueAt = '<@&806737253774524456>';
+        const leagueEmoji = '<a:ratJam:812469988266213436>';
+        const leagueName = 'ratJam';
 
-        const leagueAt = '<@&806737253774524456>'
-        const leagueEmoji = '<a:ratJam:812469988266213436>'
-        const leagueName = 'ratJam'
+        const chessPing  = msg.guild.roles.cache.find(role => role.name === "Pinged for Chess");
+        const chessAt = '<@&814421520453337109>';
+        const chessEmoji = '<a:catJam:812470198006841364>';
+        const chessName = 'catJam';
 
         let embed = new Discord.MessageEmbed()
             .setColor('#CC7900')
             .setTitle('React to get your roles')
-            .setDescription(`This will allow you to get pings\n\n ${leagueEmoji} ${leagueAt}`);
-        console.log("how many times");
+            .setDescription(`This will allow you to get pings\n\n ${leagueEmoji} ${leagueAt} \n\n  ${chessEmoji} ${chessAt}`);
+
         let messageEmbed = await msg.channel.send(embed);
         messageEmbed.react(`${leagueEmoji}`);
+        messageEmbed.react(`${chessEmoji}`);
 
         msg.client.on('messageReactionAdd', async (reaction, user) => {
             if (reaction.message.partial) await reaction.message.fetch();
@@ -36,6 +42,9 @@ module.exports = async function (msg, args, cmd, Discord) {
             if (reaction.message.channel.id === channel) {
                 if (reaction.emoji.name === `${leagueName}`) {
                     await reaction.message.guild.members.cache.get(user.id).roles.add(leaguePing);
+                }
+                else if (reaction.emoji.name === `${chessName}`) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(chessPing);
                 }
             } else {
                 return;
@@ -53,6 +62,9 @@ module.exports = async function (msg, args, cmd, Discord) {
             if (reaction.message.channel.id === channel) {
                 if (reaction.emoji.name === `${leagueName}`) {
                     await reaction.message.guild.members.cache.get(user.id).roles.remove(leaguePing);
+                }
+                else if (reaction.emoji.name === `${chessName}`) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(chessPing);
                 }
             } else {
                 return;
